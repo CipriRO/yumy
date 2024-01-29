d0e59415ddedd51b67ca5b9b0924ccad856c6b63
Stefan Ciprian <stefanciprian444@gmail.com>
2024-01-29 22:09:25 +0200

    Responsive design improved and small modifications on the navbar

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,18 +3,22 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <section className="flex items-center lg:mx-16 mx-5 mr-7 my-24 lg:my-32">
-      <div className="flex flex-col justify-center items-start md:w-4/6 sm:w-5/6">
+    <section className="flex items-center lg:mx-16 mx-5 mr-7 my-28 lg:my-32 sm:my-32">
+      <div className="flex flex-col justify-center items-start md:w-4/6 sm:w-11/12">
         <h1 className="text-pretty lg:text-6xl text-5xl font-black text-text mb-3">
           Discover Something Yummy Every Day!
         </h1>
 
-        <p className="text-balance mb-8 text-text-700 w-5/6">
-          Connect, cook, and make new friends with our social media platform
-          dedicated to sharing delicious recipes!
+        <p className="text-balance mb-8 text-text-700 w-9/12">
+          Connect, cook, and create delicious memories with our social
+          recipe-sharing platform!
         </p>
 
-        <Link href="/login" role="button" className="py-3 px-16 font-semibold text-slate-50 rounded-full bg-primary-500 shadow-lg shadow-primary hover:-translate-y-1 hover:scale-105 transition-transform">
+        <Link
+          href="/login"
+          role="button"
+          className="py-3 px-16 font-semibold text-slate-50 rounded-full bg-primary-500 shadow-lg shadow-primary hover:-translate-y-1 hover:scale-105 transition-transform"
+        >
           Get started
         </Link>
       </div>
@@ -22,7 +26,7 @@ const Hero = () => {
       <Image
         src="/images/hero.png"
         alt="Image of a sallad"
-        className="drop-shadow-[0px_0px_10px_#212922] -z-20 absolute lg:right-0 sm:-right-52 md:-right-40 -right-64"
+        className="drop-shadow-[0px_0px_10px_#212922] -z-20 absolute lg:right-4 sm:-right-52 md:-right-40 -right-64"
         height={500}
         width={500}
         priority
--- app/components/NavLinks.jsx
+++ app/components/NavLinks.jsx
@@ -7,15 +7,25 @@ const NavLinks = () => {
   const pathname = usePathname();
 
   return (
-    <ul className="flex gap-6">
-      <li className={pathname === "/" && "text-primary font-semibold"}>
-        <Link href="/">Home</Link>
+    <ul className="flex gap-8">
+      <li className={pathname === "/" && "text-primary-500 font-semibold"}>
+        <Link href="/" role="button">
+          Home
+        </Link>
       </li>
       <li className={pathname === "/discover" && "text-primary font-semibold"}>
-        <Link href="/discover">Discover</Link>
+        <Link href="/discover" role="button">
+          Discover
+        </Link>
       </li>
-      <li className={pathname === "/login" && "text-primary font-semibold"}>
-        <Link href="/login">Log In</Link>
+      <li className={pathname === "/login" && "text-primary"}>
+        <Link
+          href="/login"
+          role="button"
+          className="rounded-full py-2 px-6 shadow-lg bg-primary-300/50 border-2 -ml-2 border-primary-300 text-primary-600 font-medium hover:bg-primary-500/80 hover:border-primary-500/80 hover:text-slate-50 transition-colors"
+        >
+          Log In
+        </Link>
       </li>
     </ul>
   );
