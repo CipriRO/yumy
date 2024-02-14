494324fa676eb685f5e0c8b1f3a3c43d688ccbc4
Stefan Ciprian <stefanciprian444@gmail.com>
2024-02-14 19:42:57 +0200

    Styled a bit the Community section by adding more recipes. There is a bug that needs to be fixed there. Fixing it is expected soon..

--- app/components/Community.jsx
+++ app/components/Community.jsx
@@ -9,26 +9,19 @@ const Community = () => {
         Unite
       </h2>
 
-      <div className="relative bg-[url('/images/recipe-of-the-year.jpg')] bg-cover bg-center w-full max-w-[35rem] aspect-video rounded-3xl mb-2 shadow overflow-hidden -z-10">
-        <div className="flex justify-between absolute top-0 left-0 right-0 px-5 pb-7 pt-4 bg-gradient-to-b from-[#000000c7] to-transparent">
-          <p className="font-medium text-[#ECC209] flex justify-center w-full items-center gap-1 mb-1">
-            <StarIcon className="w-5" /> Recipe of the year 2023
-          </p>
-        </div>
-
-        <div className="flex justify-between absolute bottom-0 left-0 right-0 px-5 pb-4 pt-14 bg-gradient-to-t from-[#000000c7] to-transparent">
-          <div>
-            <h4 className="font-semibold text-lg text-background-50">
-              Ziti-Style Spaghetti Squash
-            </h4>
-            <p className="text-background-300">Jenny Buckson</p>
-          </div>
-
-          <div className="flex gap-1 items-center text-[#E94959]">
-            <HeartIcon className="w-6" />
-            <span className="font-medium">91.3M</span>
-          </div>
-        </div>
+      <div className="relative flex flex-col w-full aspect-video">
+        {Recipes.map((recipe, index) => (
+          <RecipeCard
+            key={index}
+            index={index}
+            name={recipe.name}
+            autor={recipe.autor}
+            image={recipe.image}
+            likes={recipe.likes}
+            style={`top: ${index*30}`}
+            length={Recipes.length}
+          />
+        ))}
       </div>
 
       {/* <p className="text-text-700 md:w-10/12">
@@ -42,3 +35,56 @@ const Community = () => {
 };
 
 export default Community;
+
+const RecipeCard = ({ name, autor, image, likes, index, style, length }) => {
+  return (
+    <div
+      style={`background-image: url(/images/${image}); ${style}`}
+      className={`absolute bg-cover bg-center w-full max-w-[35rem] aspect-video rounded-3xl mb-2 shadow-lg overflow-hidden -z-10`}
+    >
+      {index && length && index === length - 1 && (
+        <div className="flex justify-between absolute top-0 left-0 right-0 px-5 pb-7 pt-4 bg-gradient-to-b from-[#000000c7] to-transparent">
+          <p className="font-medium text-[#ECC209] flex justify-center w-full items-center gap-1 mb-1">
+            <StarIcon className="w-5" /> Recipe of the year 2023
+          </p>
+        </div>
+      )}
+
+      <div className="flex justify-between absolute bottom-0 left-0 right-0 px-5 pb-4 pt-14 bg-gradient-to-t from-[#000000c7] to-transparent">
+        <div>
+          <h4 className="font-semibold sm:text-xl text-lg text-background-50">
+            {name}
+          </h4>
+          <p className="text-background-300">{autor}</p>
+        </div>
+
+        <div className="flex gap-1 items-center text-[#E94959]">
+          <HeartIcon className="w-6" />
+          <span className="font-medium">{likes}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Recipes = [
+  {
+    name: "Grill Chicken",
+    autor: "Emily Rodriguez",
+    likes: "10.2M",
+    image: "recipe-community-2.jpg",
+  },
+  {
+    name: "Savory Dutch Baby",
+    autor: "Adam Patel",
+    likes: "982K",
+    image: "recipe-community-1.jpg",
+  },
+  {
+    name: "Ziti-Style Spaghetti Squash",
+    autor: "Sophia Nguyen",
+    likes: "91.3M",
+    middle: true,
+    image: "recipe-of-the-year.jpg",
+  },
+];
--- app/components/NavLinks.jsx
+++ app/components/NavLinks.jsx
@@ -8,12 +8,12 @@ const NavLinks = () => {
 
   return (
     <ul className="flex gap-8">
-      <li className={pathname === "/" && "text-primary-500 font-semibold"}>
+      <li className={pathname === "/" ? "text-primary-500 font-semibold" : undefined}>
         <Link href="/" role="button">
           Home
         </Link>
       </li>
-      <li className={pathname === "/discover" && "text-primary font-semibold"}>
+      <li className={pathname === "/discover" ? "text-primary font-semibold" : undefined}>
         <Link href="/discover" role="button">
           Discover
         </Link>
@@ -22,7 +22,7 @@ const NavLinks = () => {
         <Link
           href="/login"
           role="button"
-          className={`rounded-full py-2 px-6 shadow-lg bg-primary-300/50 border-2 -ml-2 border-primary-300 text-primary-600 font-medium hover:bg-primary-500/80 hover:border-primary-500/80 hover:text-slate-50 transition-colors ${pathname === "/login" && "!bg-primary-500/80 !border-primary-500/80 !text-slate-50"}`}
+          className={`rounded-full py-2 px-6 shadow-lg bg-primary-300/50 border-2 -ml-2 border-primary-300 text-primary-600 font-medium hover:bg-primary-500/80 hover:border-primary-500/80 hover:text-slate-50 transition-colors ${pathname === "/login" ? "!bg-primary-500/80 !border-primary-500/80 !text-slate-50" : undefined}`}
         >
           Log In
         </Link>
