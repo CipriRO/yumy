b039fc8000dbb8e1b776199eea41304321c0961a
Stefan Ciprian <stefanciprian444@gmail.com>
2024-08-15 16:31:22 +0300

    perf: add image optimisation for the recipes on the home screen

--- app/components/Home/Helper/HomeRecipeCard.jsx
+++ app/components/Home/Helper/HomeRecipeCard.jsx
@@ -1,16 +1,26 @@
 import { cn } from "@/app/lib/utils";
 import { HeartIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
-const HomeRecipeCard = ({ recipe, style, small }) => {
-  const { name, likes, image, user } = recipe;
+const HomeRecipeCard = ({ recipe, small }) => {
+  const { name, likes, image, blurImage, user } = recipe;
 
   return (
     <article
-      style={{ backgroundImage: `url(${image})`, ...style }}
-      className={`relative w-full grow bg-cover bg-center text-slate-100 shadow-xl ${
+      className={`relative w-full grow text-slate-100 shadow-xl ${
         small ? "max-w-60" : "max-w-[27rem]"
       } z-0 aspect-video flex-shrink overflow-hidden rounded-3xl shadow-lg`}
     >
+      <Image
+        src={image}
+        alt={name}
+        fill={true}
+        placeholder="blur"
+        blurDataURL={blurImage}
+        className="absolute object-cover object-center"
+        sizes="(max-width: 600px) 100vw, 50vw"
+      />
+
       <div
         className={`absolute bottom-0 left-0 right-0 flex justify-between gap-3 ${
           small ? "px-4 pb-2 pt-16" : "px-5 pb-4 pt-28"
