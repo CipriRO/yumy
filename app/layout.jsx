90667f7bda26aad1b6e564252056960ab0802e59
Stefan Ciprian <stefanciprian444@gmail.com>
2024-08-16 11:40:51 +0300

    fix: screen size component not removed correctly

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,7 +20,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-background">
       <body className={be_vietnam_pro.className}>
-        <ScreenSize />
         {children}
         <ProgressBar />
       </body>
